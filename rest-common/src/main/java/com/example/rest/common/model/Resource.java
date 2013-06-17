747aae40fd1bf2356e00da327bd9dba2e0fd2d5a
alexfdz <alexfdz@gmail.com>
2013-06-17 12:33:17 +0100

    Merge branch master

--- rest-common/src/main/java/com/example/rest/common/model/Resource.java
+++
@@ -1,9 +0,0 @@
-package com.example.rest.common.model;
-
-import org.springframework.hateoas.ResourceSupport;
-
-public abstract class Resource extends ResourceSupport{
-	
-	public abstract String getUniqueId();
-
-}
